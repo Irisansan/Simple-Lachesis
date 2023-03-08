a128d6f7e8ee126bb7dd84f6cd9cd8c5a3be6f90
machin3boy <amir.kashapov@protonmail.ch>
2023-03-08 21:46:17 +1100

    fix: correctly reading all nodes and edges into local lachesis state DAG

--- PyLachesis/Lachesis.py
+++ PyLachesis/Lachesis.py
@@ -2,8 +2,8 @@ import DAG as dag
 import glob
 import networkx as nx
 
+# iterating over all graphs as part of testing, etc.
 """
-#iterating over all graphs as part of testing, etc.
 graphs_directory = "inputs/graphs/graph_*.txt"
 
 for filename in glob.glob(graphs_directory):
@@ -17,19 +17,57 @@ class Lachesis:
         self.block = 0
         self.frame = 0
         self.epoch = 0
-        self.root_set = []
+        self.root_sets = {}  # {root_set[i] => roots} for i in range(frame_numbers)
         self.cheater_list = []
         self.atropos_list = []
-        self.validator = validator
+        self.validators = []
         self.adjacency_matrix = []
         self.time = 0
         self.global_vector = []
         self.events_at_step = []
         # combining DAGs will eventually be required
         self.local_dag = nx.DiGraph()
+        # self.local_rich_dag - DAG with rich logging (block, frame, etc.) for events
+        self.timestep_graph = (
+            None  # - gDAG nodes at any given concurrent logical timestep
+        )
 
     def check_for_roots(self):
-        return
+        print(self.local_dag)
+        print("check_for_roots")
+        for node in self.timestep_graph.nodes(data=True):
+            print(node)
+            if self.local_dag.has_node(node):
+                local_dag_node = self.local_dag.nodes[node]
+                print("LDN", local_dag_node)
+                predecessors = local_dag_node["predecessors"]
+                if predecessors == 0:
+                    if self.frame in self.root_sets:
+                        self.root_sets[self.frame].append(node)
+
+                validators_observed = local_dag_node["validators_observed"]
+                for source, target in self.local_dag.out_edges(local_dag_node[0]):
+                    source_data = self.local_dag.nodes[source]
+                    target_data = self.local_dag.nodes[target]
+                    target_validator = target[0]
+                    print(validators_observed, target_validator)
+                    if target_validator not in validators_observed:
+                        source_validator = source[0]
+                        if source_validator != target_validator:
+                            target_weight = target_data["weight"]
+                            if (
+                                target_validator
+                                in source_data["validators_observed"]
+                            ):
+                                source_data["validators_observed"][
+                                    target_validator
+                                ] += target_weight
+                            else:
+                                source_data["validators_observed"][
+                                    target_validator
+                                ] = target_weight
+                        validators_observed.add(target_validator)
+                        print("observed", validators_observed)
 
     """
     def elect_atropos(self):
@@ -45,31 +83,57 @@ class Lachesis:
 # Lachesis on one node
 def process_graph_by_timesteps(graph):
     lachesis_state = Lachesis()
-    nodes = iter(graph.nodes(data=True))
+    nodes = iter(
+        sorted(graph.nodes(data=True), key=lambda node: node[1]["timestamp"])
+    )
     node = next(nodes, None)
-    timestep_graph = nx.DiGraph()  # initialize the minigraph
-    while node is not None:
+    lachesis_state.timestep_graph = nx.DiGraph()  # nodes at the current timestep
+    while True:
+        if node is None:
+            break
         validator = node[0][0]
         timestamp = node[0][1]
-        if timestamp == lachesis_state.time:
-            # add node to the minigraph
-            timestep_graph.add_node((validator, timestamp))
-            # add outward edges to the minigraph
-            for source, target in graph.out_edges((validator, timestamp)):
-                timestep_graph.add_edge(source, target)
-            node = next(nodes, None)
-        else:
-            # end of timestep, combine minigraph with local_dag
-            lachesis_state.local_dag = nx.compose(
-                lachesis_state.local_dag, timestep_graph
+        if validator not in lachesis_state.validators:
+            lachesis_state.validators.append(validator)
+        while timestamp == lachesis_state.time:
+            # add node to graph of current nodes and local DAG
+            lachesis_state.timestep_graph.add_node(
+                (validator, timestamp),
+                timestamp=timestamp,
+                predecessors=node[1]["predecessors"],
+                weight=node[1]["weight"],
+                validators_observed=node[1]["validators_observed"],
             )
+            lachesis_state.local_dag.add_node(
+                (validator, timestamp),
+                timestamp=timestamp,
+                predecessors=node[1]["predecessors"],
+                weight=node[1]["weight"],
+                validators_observed=node[1]["validators_observed"],
+            )
+
+            # Add edges from this node to its successors
+            for successor in graph.successors((validator, timestamp)):
+                lachesis_state.local_dag.add_edge((validator, timestamp), successor)
+
+            node = next(nodes, None)
+            if node is None:
+                break
+            validator = node[0][0]
+            timestamp = node[0][1]
+        else:
             lachesis_state.time += 1
-            timestep_graph = nx.DiGraph()  # reset the minigraph
-    # combine the last minigraph with local_dag
-    # (in case the graph didn't end on a timestep)
-    lachesis_state.local_dag = nx.compose(lachesis_state.local_dag, timestep_graph)
-    # print(lachesis_state.local_dag.edges())  # print the edges of local_dag
-    # print(lachesis_state.local_dag.nodes())  # print the nodes of local_dag
+            lachesis_state.timestep_graph = nx.DiGraph()
+
+    G = lachesis_state.local_dag
+
+    print("Nodes in the graph:")
+    for node in G.nodes(data=True):
+        print(node)
+
+    print("Edges in the graph:")
+    for edge in G.edges():
+        print(edge)
 
 
 if __name__ == "__main__":
