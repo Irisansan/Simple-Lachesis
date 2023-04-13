b39949d36835e7dff30b3ae26da5523419e82439
machin3boy <amir.kashapov@protonmail.ch>
2023-04-14 03:12:37 +1000

    feat: adjacency matrices for neighbors and generalized graphing function

--- inputs/graph.py
+++ inputs/graph.py
@@ -12,15 +12,34 @@ def createGraph(
     num_nodes,
     node_present_probability,
     observing_probability,
+    neighbor_probability,
     annotate=False,
     show_graph=False,
     save_plot=False,
     graph_filename="graph.png",
     txt_filename_format_one="txt.txt",
     txt_filename_format_two="txt2.txt",
+    neighbor_filename="neighbors.txt",
 ):
     # Initialize the graph
     G = nx.DiGraph()
+
+    # Create the neighbors dictionary
+    neighbors = {}
+    for i in range(num_nodes):
+        for j in range(i + 1, num_nodes):
+            if random.random() < neighbor_probability:
+                if i not in neighbors:
+                    neighbors[i] = set()
+                if j not in neighbors:
+                    neighbors[j] = set()
+                neighbors[i].add(j)
+                neighbors[j].add(i)
+
+    # Save neighbors dictionary to a file
+    with open(neighbor_filename, "w") as f:
+        for key, values in neighbors.items():
+            f.write(f"{key}: {', '.join(str(value) for value in values)}\n")
 
     # Add nodes and assign colors
     colors = [
@@ -84,14 +103,16 @@ def createGraph(
                 parent = (i - 1, j)
                 self_ref = False
                 while not self_ref and parent[0] >= 0:
-                    if parent in G.nodes:
+                    if parent in G.nodes and parent[1] in neighbors.get(node[1], set()):
                         G.add_edge(node, parent)
                         self_ref = True
                     parent = (parent[0] - 1, parent[1])
                 for k in range(num_nodes):
                     if random.random() < observing_probability:
                         target = (i - 1, k)
-                        if target in G.nodes:
+                        if target in G.nodes and target[1] in neighbors.get(
+                            node[1], set()
+                        ):
                             G.add_edge(node, target)
 
     labels = {
@@ -264,105 +285,125 @@ def createGraph(
     plt.close()
 
 
-if __name__ == "__main__":
-    annotate_graph = input("Annotate graphs (y/n): (Default is no) ")
-    if annotate_graph == "y" or annotate_graph == "Y":
-        annotate = True
-    else:
-        annotate = False
-    num_graphs = input("How many graphs would you like to generate: (Default is 50) ")
-    if num_graphs == "":
-        num_graphs = 50
-    else:
-        num_graphs = int(num_graphs)
-
-    cheater_input = input(
-        "Enter the probability that a random validator node is a cheater: (Default is 0.2) "
+def generate_graphs(
+    annotate_graph,
+    num_graphs,
+    cheater_input,
+    level_input,
+    node_input,
+    present_prob_input,
+    observe_prob_input,
+    neighbor_prob_input,
+    base_dir,
+    starting_index,
+):
+    annotate = True if annotate_graph.lower() == "y" else False
+    num_graphs = int(num_graphs) if num_graphs else 50
+    cheater_input = float(cheater_input) if cheater_input else 0.2
+    level_input = (
+        int(level_input)
+        if level_input and level_input.lower() not in ["r", "random"]
+        else None
     )
-    if cheater_input == "":
-        cheater_input = 0.2
-    else:
-        cheater_input = float(cheater_input)
-
-    level_input = input(
-        "Enter the number of levels in each graph or type 'r' or 'random' for a random value each iteration: (Default is 10) "
+    node_input = (
+        int(node_input)
+        if node_input and node_input.lower() not in ["r", "random"]
+        else None
     )
-    if level_input.lower() in ["r", "random"]:
-        level_input = None
-    else:
-        if level_input == "":
-            level_input = 10
-        else:
-            level_input = int(level_input)
-
-    node_input = input(
-        "Enter the number of nodes in each level or type 'r' or 'random' for a random value each iteration: (Default is 5) "
+    present_prob_input = (
+        float(present_prob_input)
+        if present_prob_input and present_prob_input.lower() not in ["r", "random"]
+        else None
     )
-    if node_input.lower() in ["r", "random"]:
-        node_input = None
-    else:
-        if node_input == "":
-            node_input = 5
-        else:
-            node_input = int(node_input)
-
-    present_prob_input = input(
-        "Enter the probability that a node is present or type 'r' or 'random' for a random value each iteration: (Default is 0.65) "
+    observe_prob_input = (
+        float(observe_prob_input)
+        if observe_prob_input and observe_prob_input.lower() not in ["r", "random"]
+        else None
     )
-    if present_prob_input.lower() in ["r", "random"]:
-        present_prob_input = None
-    else:
-        if present_prob_input == "":
-            present_prob_input = 0.65
-        else:
-            present_prob_input = float(present_prob_input)
-
-    observe_prob_input = input(
-        "Enter the probability that a node observes another validator or type 'r' or 'random' for a random value each iteration: (Default is 0.3) "
+    neighbor_prob_input = (
+        float(neighbor_prob_input)
+        if neighbor_prob_input and neighbor_prob_input.lower() not in ["r", "random"]
+        else None
     )
-    if observe_prob_input.lower() in ["r", "random"]:
-        observe_prob_input = None
-    else:
-        if observe_prob_input == "":
-            observe_prob_input = 0.3
-        else:
-            observe_prob_input = float(observe_prob_input)
 
     for i in range(num_graphs):
-        # print("GRAPH", i+1)
-        if level_input is None:
-            num_levels = random.randint(5, 100)
-        else:
-            num_levels = level_input
+        num_levels = random.randint(5, 100) if level_input is None else level_input
+        num_nodes = random.randint(3, 25) if node_input is None else node_input
+        node_present_probability = (
+            random.uniform(0.5, 0.7)
+            if present_prob_input is None
+            else present_prob_input
+        )
+        observing_probability = (
+            random.uniform(0.2, 0.4)
+            if observe_prob_input is None
+            else observe_prob_input
+        )
+        neighbor_probability = (
+            random.uniform(0.1, 0.9)
+            if neighbor_prob_input is None
+            else neighbor_prob_input
+        )
 
-        if node_input is None:
-            num_nodes = random.randint(3, 25)
-        else:
-            num_nodes = node_input
-
-        if present_prob_input is None:
-            node_present_probability = random.uniform(0.5, 0.7)
-        else:
-            node_present_probability = present_prob_input
-
-        if observe_prob_input is None:
-            observing_probability = random.uniform(0.2, 0.4)
-        else:
-            observing_probability = observe_prob_input
-
-        graph_filename = f"graphs_with_cheaters/graph_{i+51}.pdf"
-        txt_filename_format_one = f"graphs_with_cheaters/graph_{i+51}.txt"
-        txt_filename_format_two = f"graphs_with_cheaters/events_{i+51}.txt"
+        graph_filename = f"{base_dir}/graph_{starting_index + i}.pdf"
+        txt_filename_format_one = f"{base_dir}/graph_{starting_index + i}.txt"
+        txt_filename_format_two = f"{base_dir}/events_{starting_index + i}.txt"
+        neighbor_filename = f"{base_dir}/neighbors_{starting_index + i}.txt"
         createGraph(
             cheater_input,
             num_levels,
             num_nodes,
             node_present_probability,
             observing_probability,
+            neighbor_probability,
             annotate,
             show_graph=False,
             save_plot=True,
             graph_filename=graph_filename,
             txt_filename_format_one=txt_filename_format_one,
             txt_filename_format_two=txt_filename_format_two,
+            neighbor_filename=neighbor_filename,
         )
+
+
+if __name__ == "__main__":
+    # Collect input from the user
+    annotate_graph = input("Annotate graphs (y/n): (Default is no) ")
+    num_graphs = input("How many graphs would you like to generate: (Default is 50) ")
+    cheater_input = input(
+        "Enter the probability that a random validator is a cheater: (Default is 0.2) "
+    )
+    level_input = input(
+        "Enter the number of levels/time steps in each graph or type 'r' or 'random' for a random value each iteration: (Default is 10) "
+    )
+    node_input = input(
+        "Enter the number of validator event nodes in each level or type 'r' or 'random' for a random value each iteration: (Default is 5) "
+    )
+    present_prob_input = input(
+        "Enter the probability that an event node is present or type 'r' or 'random' for a random value each iteration: (Default is 0.65) "
+    )
+    observe_prob_input = input(
+        "Enter the probability that an event node observes another validator's event node or type 'r' or 'random' for a random value each iteration: (Default is 0.3) "
+    )
+    neighbor_prob_input = input(
+        "Enter the probability that any two given validators are neighbors or type 'r' or 'random' for a random value each iteration: (Default is 0.5) "
+    )
+    base_dir = input("Enter the base directory for output files: ")
+    starting_index = input(
+        "Enter the starting index for file numbering: (Default is 1) "
+    )
+
+    starting_index = int(starting_index) if starting_index else 1
+
+    generate_graphs(
+        annotate_graph,
+        num_graphs,
+        cheater_input,
+        level_input,
+        node_input,
+        present_prob_input,
+        observe_prob_input,
+        neighbor_prob_input,
+        base_dir,
+        starting_index,
+    )
