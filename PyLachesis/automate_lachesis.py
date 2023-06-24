4f5bc8b7bc996f55b5e6083659ba6681871e45a8
machin3boy <amir.kashapov@protonmail.ch>
2023-06-24 21:41:50 +1000

    feat: success rate and error info for automated test runs

--- PyLachesis/automate_lachesis.py
+++ PyLachesis/automate_lachesis.py
@@ -3,14 +3,11 @@ import os
 from tqdm import tqdm
 from lachesis import Lachesis, LachesisMultiInstance
 
-import os
-
 
 def create_dir(path):
     try:
         os.makedirs(path)
     except FileExistsError:
-        # directory already exists
         pass
 
 
@@ -20,30 +17,42 @@ def automate_lachesis(input_dir, output_dir, create_graph=False):
 
     print(f"processing {len(file_list)} files...")
 
+    success_count = 0
+
     for _, input_filename in tqdm(
         enumerate(file_list), total=len(file_list), desc="processing files"
     ):
-        base_filename = os.path.basename(input_filename)
-        graph_name = base_filename[
-            base_filename.index("_") + 1 : base_filename.index(".txt")
-        ]
+        try:
+            base_filename = os.path.basename(input_filename)
+            graph_name = base_filename[
+                base_filename.index("_") + 1 : base_filename.index(".txt")
+            ]
 
-        # create a directory for each graph
-        graph_dir = os.path.join(output_dir, f"graph_{graph_name}_results")
-        create_dir(graph_dir)
+            graph_dir = os.path.join(output_dir, f"graph_{graph_name}_results")
+            create_dir(graph_dir)
 
-        output_filename = os.path.join(graph_dir, "result.pdf")
+            output_filename = os.path.join(graph_dir, "result.pdf")
 
-        # Single instance run
-        lachesis_state = Lachesis()
-        lachesis_state.run_lachesis(input_filename, output_filename, create_graph)
+            lachesis_state = Lachesis()
+            lachesis_state.run_lachesis(input_filename, output_filename, create_graph)
 
-        # Multiinstance run
-        lachesis_multi_instance = LachesisMultiInstance(graph_results=create_graph)
-        lachesis_multi_instance.run_lachesis_multiinstance(input_filename, graph_dir)
+            lachesis_multi_instance = LachesisMultiInstance(graph_results=create_graph)
+            lachesis_multi_instance.run_lachesis_multiinstance(
+                input_filename, graph_dir
+            )
+
+            success_count += 1
+
+        except Exception as e:
+            print("error in", graph_name, str(e))
+
+    success_rate = success_count / len(file_list) * 100
+    print(f"success rate: {success_rate:.1f}%")
 
 
-print("\nautomating graphs without cheaters...\n\n")
-automate_lachesis("../inputs/graphs", "../inputs/results", True)
+# print("\nautomating graphs without cheaters...\n\n")
+# automate_lachesis("../inputs/graphs", "../inputs/results", False)
 print("\n\nautomating graphs with cheaters...\n\n")
-automate_lachesis("../inputs/cheaters", "../inputs/cheater_results", True)
+automate_lachesis(
+    "../inputs/cheaters_expanded", "../inputs/cheaters_expanded_results", False
+)
